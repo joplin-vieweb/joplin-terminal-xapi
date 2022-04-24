4861fe81222acdfe9c7267d02d59d17ff3b7e769
gri38 <onzeweb@zaclys.net>
2022-04-24 16:20:24 +0200

    Add synchro logic

--- node-server/joplin.js
+++ node-server/joplin.js
@@ -1,6 +1,36 @@
 const express = require('express');
 const router = express.Router();
 const child_process = require('child_process');
+const sync = require('./synch');
+
+function get_joplin_conf() {
+    var get_synch_data = child_process.spawnSync("/app/joplin/bin/joplin", ["config"], { encoding : 'utf8' });
+    if (get_synch_data.error) {
+        return get_synch_data;
+    }
+
+    const ini = require('ini');
+    let config = ini.parse(get_synch_data.stdout);
+    console.log(config);
+    
+    let target = config["sync.target"];
+    let path = config["sync.5.path"];
+    let user = config["sync.5.username"];
+    let interval = config["sync.interval"];
+    let password = config["sync.5.password"];
+
+    get_synch_data.config = {
+            'target': target ? parseInt(target) : 0,
+            'path': path ? path : "",
+            'user': user ? user : "",
+            'interval': interval ? parseInt(interval) : 0,
+            'password': password ? password : ''
+        };
+
+    return get_synch_data;
+}
+
+sync.set_interval(get_joplin_conf().config["interval"])
 
 router.get('/version', (req, res) => {
     var get_version = child_process.spawnSync("/app/joplin/bin/joplin", ["version"], { encoding : 'utf8' });
@@ -13,29 +43,14 @@ router.get('/version', (req, res) => {
 });
 
 router.get('/config', (req, res) => {
-    var get_synch_data = child_process.spawnSync("/app/joplin/bin/joplin", ["config"], { encoding : 'utf8' });
+    var get_synch_data = get_joplin_conf();
+    // var get_synch_data = child_process.spawnSync("/app/joplin/bin/joplin", ["config"], { encoding : 'utf8' });
     if(get_synch_data.error) {
         res.status(500).send(get_synch_data.error);
         return;
     }
-    
-    const ini = require('ini');
-    let config = ini.parse(get_synch_data.stdout);
-    console.log(config);
-    
-    let target = config["sync.target"];
-    let path = config["sync.5.path"];
-    let user = config["sync.5.username"];
-    let interval = config["sync.interval"];
-    let password = config["sync.5.password"];
-    
-    res.json({
-        'target': target ? parseInt(target) : 0,
-        'path': path ? path : "",
-        'user': user ? user : "",
-        'interval': interval ? parseInt(interval) : 0,
-        'password': password ? password : ''
-    });
+
+    res.json(get_synch_data.config);
 });
 
 function set_joplin_conf(param, value) {
@@ -60,7 +75,10 @@ router.post('/config', function(req, res){
     if (path) set_joplin_conf("sync.5.path", path);
     if (user) set_joplin_conf("sync.5.username", user);
     if (password) set_joplin_conf("sync.5.password", password);
-    if (interval) set_joplin_conf("sync.interval", interval);
+    if (interval) {
+        set_joplin_conf("sync.interval", interval);
+        sync.set_interval(parseInt(interval));
+    }
 
     res.send({"status": true, "message": ""});
 });
@@ -112,6 +130,27 @@ router.post('/config/test', async (req, res, next) => {
         return res.send({"status": false, "message": err.message});
         // return res.status(500).send(err.message);
     }
+});
+
+router.post('/synch', async (req, res) => {
+    // lancer dans un thread la synchro, qui devra retourner info, output et error pour get synch
+    sync.start();
+    res.send("Sync started")
+});
+
+router.get('/synch', async (req, res) => {
+    let status;
+    let output;
+    let error;
+    await sync.get_error().then((data) => {error = data} );
+    await sync.get_output().then((data) => {output = data} );
+    await sync.get_status().then((data) => {status = data} );
+    return res.send({
+                        "info": status,
+                        "output": output,
+                        "error": error
+                    });
+
 });
 
 
---
+++ node-server/synch.js
@@ -0,0 +1,120 @@
+var spawn = require('child_process').spawn;
+var Mutex = require('async-mutex').Mutex;
+var CronJob = require('cron').CronJob;
+
+class Sync {
+    constructor() {
+        this.status = "?";
+        this.output = "";
+        this.error = "";
+        this.mutex = new Mutex();
+
+        this.job = new CronJob('00 00 00 * * *', () => { this.start(); });
+    }
+
+    set_interval(interval) {
+        this.job.stop();
+        console.log("sync job stopped");
+        let start = false;
+        let cron_syntax = "";
+        console.log("Set sync interval: " + interval);
+        switch (interval) {
+            case 300: // 5 minutes
+                cron_syntax = '0 */5 * * * *';
+                start = true;
+                break;
+            case 600: // 10 mintes
+                cron_syntax = '0 */10 * * * *';
+                start = true;
+                break;
+            case 1800: // 30 min
+                cron_syntax = '0 */30 * * * *';
+                start = true;
+                break;
+            case 3600: // 1 hour
+                cron_syntax = '0 0 * * * *';
+                start = true;
+                break;
+            case 43200: // 12 hours
+                cron_syntax = '0 0 */12 * * *';
+                start = true;
+                break;
+            case 86400: // 24 hours
+                cron_syntax = '0 0 0 * * *';
+                start = true;
+                break;
+        }
+        if (start) {
+            console.log("start sync job with cron " + cron_syntax);
+            this.job = new CronJob(cron_syntax, () => { this.start(); });
+            this.job.start();
+        }
+    }
+    
+    async start() {
+        this.mutex.runExclusive(() => {
+            if (this.status == "ongoing") {
+                return;
+            }
+            this.status = "ongoing";
+            this.output = "";
+            this.error = "";
+    
+            let synch_command = spawn("/app/joplin/bin/joplin", ["sync"], { encoding : 'utf8' });
+            synch_command.stdout.setEncoding('utf8');
+            synch_command.stderr.setEncoding('utf8');
+            synch_command.stdout.on('data', async (data) => {this.on_output(data)});
+            synch_command.stderr.on('data', async (data) => {this.on_output(data)});
+            synch_command.on('close', async (code) => {this.on_sync_finished(code)});
+        });
+    }
+
+    async on_output(data) {
+        this.mutex.runExclusive(() => {
+            this.output += data.toString();
+        });
+    }
+    
+    async on_error(data) {
+        this.mutex.runExclusive(() => {
+            this.error += data.toString();
+        });
+    }
+    
+    async on_sync_finished(code) {
+        this.mutex.runExclusive(async () => {
+            this.status = (new Date()).toISOString().replace('T', ' ').substring(0, 16);
+        }).then(async () => {
+            console.log('Synch finished: return code: ' + code);
+            let status;
+            let output;
+            let error;
+            await this.get_error().then((data) => {error = data} );
+            await this.get_output().then((data) => {output = data} );
+            await this.get_status().then((data) => {status = data} );
+            console.log('    status: ' + status);
+            console.log('    stdout: ' + output);
+            console.log('    stderr: ' + error);
+        });
+    }
+    
+    async get_status() {
+        let status = "no";
+        await this.mutex.runExclusive(()=>{status = this.status});
+        return status;
+    }
+    
+    async get_output() {
+        let out = "no";
+        await this.mutex.runExclusive(()=>{out = this.output});
+        return out;
+    }
+    
+    async get_error() {
+        let error = "no";
+        await this.mutex.runExclusive(()=>{error = this.error});
+        return error;
+    }
+}
+  
+module.exports = new Sync()
